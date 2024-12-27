a10c6178862dbc114aa311259649140f0b3a5969
viktorhuygeb <viktorhuygebaert04@gmail.com>
2024-12-27 11:49:47 +0100

    Initial Commit

---
+++ JRK_IOS/JRK_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JRK_IOSApp.swift
+//  JRK_IOS
+//
+//  Created by Viktor Huygebaert on 27/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct JRK_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JRK_IOSTests/JRK_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  JRK_IOSTests.swift
+//  JRK_IOSTests
+//
+//  Created by Viktor Huygebaert on 27/12/2024.
+//
+
+import Testing
+@testable import JRK_IOS
+
+struct JRK_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
