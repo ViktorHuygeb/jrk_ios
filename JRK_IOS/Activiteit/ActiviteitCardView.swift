540b176fb4f0b6a6dba2cfd3ca3e5f91294f14dd
viktorhuygeb <viktorhuygebaert04@gmail.com>
2024-12-28 12:26:49 +0100

    Added an edit view and sheet to the detail of an activiteit

---
+++ JRK_IOS/Activiteit/ActiviteitCardView.swift
@@ -0,0 +1,42 @@
+//
+//  ActiviteitCard.swift
+//  JRK_IOS
+//
+//  Created by Viktor Huygebaert on 27/12/2024.
+//
+import SwiftUI
+import Foundation
+
+struct ActiviteitCardView: View {
+    var activiteit: Activiteit
+    private static let dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "dd/MM/yyyy"
+        return df
+    }()
+    var body: some View {
+        VStack(alignment: .leading){
+            Text(activiteit.activiteitNaam)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
+            HStack{
+                Text("\(activiteit.prijs == 0.00 ? "Gratis" : NSString(format: "€ %.2f", activiteit.prijs))")
+                    .accessibilityLabel("Deze activiteit \(activiteit.prijs == 0.00 ? "is gratis" : String(format: "kost %.2f euro", activiteit.prijs))")
+                Spacer()
+                Label("\(Self.dateFormatter.string(from: activiteit.datum))", systemImage: "calendar")
+                    .accessibilityLabel("De activiteit vindt plaats op \(Self.dateFormatter.string(from: activiteit.datum))")
+                    .labelStyle(.trailingIcon)
+                    
+            }.font(.caption)
+        }.padding()
+    }
+}
+
+struct ActiviteitCard_Previews: PreviewProvider {
+    static var activiteit = Activiteit.sampleData[0]
+    static var previews: some View {
+        ActiviteitCardView(activiteit: activiteit)
+            .previewLayout(.fixed(width: 400, height: 100))
+        
+    }
+}
---
+++ JRK_IOS/Activiteit/ActiviteitenView.swift
@@ -0,0 +1,35 @@
+//
+//  ActiviteitenView.swift
+//  JRK_IOS
+//
+//  Created by Viktor Huygebaert on 27/12/2024.
+//
+
+import SwiftUI
+
+struct ActiviteitenView: View {
+    var activiteiten: [Activiteit]
+    var body: some View {
+        NavigationStack{
+            List(activiteiten, id: \.id) {activiteit in
+                NavigationLink(destination: ActiviteitDetailView(activiteit: activiteit)){
+                    ActiviteitCardView(activiteit: activiteit)
+                }
+                .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 20))
+            }
+            .navigationTitle("Activiteiten")
+            .toolbar{
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("Voeg nieuwe activiteit toe")
+            }
+        }
+    }
+}
+
+struct ActiviteitenView_Previews: PreviewProvider{
+    static var previews: some View {
+        ActiviteitenView(activiteiten: Activiteit.sampleData)
+    }
+}
